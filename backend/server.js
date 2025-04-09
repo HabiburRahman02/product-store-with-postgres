83c015fb571f5e338e6bfdb9bf2f9b8f5f022561
Habibur Rahman <habibur88454@gmail.com>
2025-04-09 13:03:33 +0600

    route setup in backend

--- backend/server.js
+++ backend/server.js
@@ -4,19 +4,19 @@ import helmet from 'helmet';
 import dotenv from 'dotenv'
 import morgan from 'morgan';
 
+import productRoutes from './routes/productRoutes.js'
+
 dotenv.config();
 const app = express()
 const port = process.env.PORT || 3000;
 
 // middleware
+app.use(express.json());
 app.use(cors());
 app.use(helmet());
 app.use(morgan('dev'));
 
-app.get('/', (req, res) => {
-  res.send('Product store is running');
-//   console.log(res.getHeaders());
-})
+app.use("/api/products", productRoutes)
 
 app.listen(port, () => {
   console.log(`Product store is running on port ${port}`)
