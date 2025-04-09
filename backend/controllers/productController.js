af159092bef741da627fc10bdf822cd39b2f6bbf
Habibur Rahman <habibur88454@gmail.com>
2025-04-09 17:22:55 +0600

    create product api successfully added

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -13,8 +13,25 @@ export const getProducts = async(req, res) => {
   }
 };
 
-export const getProduct = async(req, res) => {};
-export const createProduct = async(req, res) => {};
+export const getProduct = async(req, res) => {
+    
+};
+export const createProduct = async(req, res) => {
+    const {name,image,price} = req.body
+    if(!name || !image || !price){
+        return res.status(400).send({success: false, message: 'Every field is required'})
+    }
+  try {
+    const newProduct = await sql`
+    INSERT INTO products(name, image, price)
+    VALUES (${name}, ${image}, ${price})
+    RETURNING *
+    `; 
+    res.status(201).json({success: true, data: newProduct})
+  } catch (error) {
+    console.log('Error createProduct', error);
+  }
+};
 export const updateProduct = async(req, res) => {};
 export const deleteProduct = async(req, res) => {};
 
